4fd82e3c3e73ad78f5e33c12c31749e631dc731e
ewen <ewen80@gmail.com>
2016-10-14 17:24:50 +0800

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+
+import {SmartadminModule} from './shared/smartadmin.module'
+import {AppComponent} from './app.component';
+
+import {routing} from './app.routing';
+import {BrowserModule} from "@angular/platform-browser";
+import {UserModule} from "./shared/user/user.module";
+import {UserService} from "./shared/user/user.service";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+
+    BrowserModule,
+
+    routing,
+
+    SmartadminModule.forRoot(),
+    UserModule.forRoot(),
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,27 @@
+/**
+ * Created by griga on 7/11/16.
+ */
+
+
+import {Routes, RouterModule} from '@angular/router';
+import {MainLayoutComponent} from "./shared/layout/app-layouts/main-layout.component";
+import {AuthLayoutComponent} from "./shared/layout/app-layouts/auth-layout.component";
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '', redirectTo: 'home', pathMatch: 'full'
+            },
+            {
+                path: 'home',
+                loadChildren: 'app/+home/home.module#HomeModule'
+            },
+        ]
+    },
+
+];
+
+export const routing = RouterModule.forRoot(routes);
