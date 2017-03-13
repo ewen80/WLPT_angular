48c7eb0b9b404e5a8f16e0625f0b1c37a6743fbf
wenliang <ewen80@gmail.com>
2017-03-13 16:49:23 +0800

    2017031301

--- src/app/shared/user/user.module.ts
+++ src/app/shared/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, ModuleWithProviders  } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { FormsModule,ReactiveFormsModule  } from "@angular/forms";
 
 import { SmartadminWidgetsModule } from '../widgets/smartadmin-widgets.module';
 import { LoginInfoComponent } from "./login-info/login-info.component";
@@ -11,7 +11,7 @@ import { RoleDetailComponent } from './roledetail/roledetail.component';
 import { UtilsModule } from '../utils/utils.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, SmartadminWidgetsModule, UtilsModule ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule ,SmartadminWidgetsModule, UtilsModule ],
   declarations: [LoginInfoComponent, LogoutComponent, LoginComponent, UserDetailComponent, RoleDetailComponent ],
   exports: [LoginInfoComponent, LogoutComponent, LoginComponent, UserDetailComponent, RoleDetailComponent ],
   
--- src/app/shared/user/userdetail/userdetail.component.ts
+++ src/app/shared/user/userdetail/userdetail.component.ts
@@ -1,5 +1,5 @@
-import {ChangeDetectorRef, Component, OnInit, Input, Output, EventEmitter, ViewChild} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component, OnInit, Input, Output, EventEmitter, ViewChild} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalModule } from 'ng2-bootstrap';
 
 
@@ -19,33 +19,35 @@ export class UserDetailComponent implements OnInit{
     @Input() saveMode: saveMode; //保存模式（只读，修改，新增）
     @Output() onSaveFinished = new EventEmitter<{saveMode:saveMode,sucess:boolean,message:string}>(); //保存完成后激活事件，参数包含保存类型（新增，修改）和保存结果以及附加消息
     
-    @ViewChild('userDetailForm') userDetailForm: NgForm;
 
-    public user: User = new User();
+    public user: User;
     public allRoles: Array<Role> = [];
     public selectedRole: Role = new Role();
 
-    constructor(private changeDetectorRef: ChangeDetectorRef, private userService: UserService, private roleService: RoleService){    }
+    userDetailForm: FormGroup;
 
-    //重置用户表状态11111111111
+    constructor(private fb: FormBuilder, private userService: UserService, private roleService: RoleService){
+        this.createForm();
+    }
+
+    private createForm(){
+        this.userDetailForm = this.fb.group({
+            name: ['', Validators.required],
+        })
+    }
+
+    //重置用户表状态
     public reset(): void{
-        if(this.userDetailForm){
-            this.userDetailForm.resetForm();
-            this.loadRoles();
-            this.selectedRole = null;
-        }
+        
     }
 
     public refresh(): void{
-        this.changeDetectorRef.detectChanges();
+        
     }
 
     //改变当前选择角色
     public changeRole(roleId:string){
-        let foundRole = this.allRoles.find( r => r.id == roleId);
-        if(foundRole){
-            this.selectedRole = foundRole;
-        }
+        
     }
 
     ngOnInit(){
