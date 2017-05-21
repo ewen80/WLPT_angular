9821412c43268baf880e00eea9f755697a21f809
ewen <ewen80@gmail.com>
2017-05-21 20:33:11 +0800

    2017050

---
+++ src/app/core/services/resources/menu.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from "rxjs/Rx";
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { AppConfig } from '../../app.config';
+import { BasicAuthenticationHttp } from '../basic-authentication-http.service';
+import { Menu } from "app/core/entity/resources/menu";
+
+@Injectable()
+export class MenuService {
+
+  private serverUrl: string = this.appConfig.getConfig("Server").Url + "/resources/menus";
+  
+  constructor(private http:BasicAuthenticationHttp, private appConfig:AppConfig){
+    console.log('MenuService created');
+  }
+
+  getAll(): Promise<Menu[]>{
+    return this.http.get(this.serverUrl)
+                      .toPromise()
+                      .then( response => response.json())   
+                      .catch(this.handleError);   
+  }
+
+  save(menu:Menu): Promise<{sucess:boolean,message:string}>{
+    return this.http.post(this.serverUrl,JSON.stringify(menu))
+                      .toPromise()
+                      .then( response => response.json())
+                      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any>{
+    // console.error('发生一个错误：', error);
+    return Promise.reject(error.message || error);
+  }
+
+}
